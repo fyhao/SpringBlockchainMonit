a2506b98b11ebb7916a70ef2304a003be7f2f0c2
fyhao <fyhao1@gmail.com>
2022-06-15 22:54:30 +0800

    TokenService define networks.json metadata to put etherscan url into config

---
+++ BlockchainMonit/src/main/java/com/fyhao/blockchainmonit/dto/BlockchainNetwork.java
@@ -0,0 +1,11 @@
+package com.fyhao.blockchainmonit.dto;
+
+import lombok.Data;
+
+@Data
+public class BlockchainNetwork {
+
+	int chainid;
+	String networkurl;
+	String tokenpath;
+}
--- BlockchainMonit/src/main/java/com/fyhao/blockchainmonit/service/TokenService.java
+++ BlockchainMonit/src/main/java/com/fyhao/blockchainmonit/service/TokenService.java
@@ -2,7 +2,6 @@ package com.fyhao.blockchainmonit.service;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,9 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fyhao.blockchainmonit.dto.BlockchainNetwork;
 import com.fyhao.blockchainmonit.dto.BlockchainToken;
 import com.fyhao.blockchainmonit.dto.EtherscanTokenItem;
 import com.google.gson.Gson;
@@ -31,6 +29,9 @@ public class TokenService {
 	@Value("classpath:tokens.json")
 	Resource tokensFile;
 	
+	@Value("classpath:networks.json")
+	Resource networksFile;
+	
 	public List<BlockchainToken> getTokens() throws IOException {
 		String data = FileUtils.readFileToString(tokensFile.getFile(), "UTF-8");
 		ObjectMapper mapper = new ObjectMapper();
@@ -41,12 +42,27 @@ public class TokenService {
 	@SneakyThrows(Exception.class)
 	public String getPrice(BlockchainToken token) {
 		RestTemplate restTemplate = new RestTemplate();
-	    final String baseUrl = String.format("https://etherscan.io/token/%s", token.getAddress());
+	    final String baseUrl = String.format(getTokenURL(token), token.getAddress());
 		URI uri = new URI(baseUrl);
 		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
 		String jsonstr = StringUtils.substringBetween(result.toString(), "<script type=\"application/ld+json\">", "</script>");
 		Gson gson = new Gson();
 		EtherscanTokenItem item = gson.fromJson(jsonstr, EtherscanTokenItem.class);
 		return item.getOffers().getPriceCurrency() + " " + item.getOffers().getPrice();	
+	}
+	
+	public List<BlockchainNetwork> getNetworks() throws IOException {
+		String data = FileUtils.readFileToString(networksFile.getFile(), "UTF-8");
+		ObjectMapper mapper = new ObjectMapper();
+		BlockchainNetwork[] networkArray = mapper.readValue(data, BlockchainNetwork[].class);
+		List<BlockchainNetwork> networkList = Arrays.asList(networkArray);
+		return networkList;
+	}
+	
+	public String getTokenURL(BlockchainToken token) throws IOException {
+		int chainid = token.getChainid();
+		BlockchainNetwork network = getNetworks().stream().filter(e -> e.getChainid() == chainid).findFirst().get();
+		String url = network.getNetworkurl() + network.getTokenpath();
+		return url;
 	}
 }
