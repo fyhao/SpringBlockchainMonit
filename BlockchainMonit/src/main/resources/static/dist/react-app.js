f46c556e35de426aa2c174f259632dcc4436dd43
fyhao <fyhao1@gmail.com>
2022-06-17 09:52:35 +0000

    add USDC ethereum

--- BlockchainMonit/src/main/resources/static/dist/react-app.js
+++ BlockchainMonit/src/main/resources/static/dist/react-app.js
@@ -809,7 +809,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\r\n#main {\r\n    margin: 0 auto;\r\n    width: 100%;\r\n}\r\n", "",{"version":3,"sources":["webpack://./src/main/webapp/css/Main.css"],"names":[],"mappings":";AACA;IACI,cAAc;IACd,WAAW;AACf","sourcesContent":["\r\n#main {\r\n    margin: 0 auto;\r\n    width: 100%;\r\n}\r\n"],"sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#main {\n    margin: 0 auto;\n    width: 100%;\n}\n", "",{"version":3,"sources":["webpack://./src/main/webapp/css/Main.css"],"names":[],"mappings":";AACA;IACI,cAAc;IACd,WAAW;AACf","sourcesContent":["\n#main {\n    margin: 0 auto;\n    width: 100%;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
